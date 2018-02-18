17b15f0644d175e0efe2f7125b8554f09418d61c
kyodgorbek <kyodgorbek@gmail.com>
2018-02-18 21:26:04 +0100

    I have added permission to android manifest

---
+++ app/src/main/java/com/edgar/yodgorbekkomilo/newsapp/Adapter/ArticleAdapter.java
@@ -0,0 +1,82 @@
+package com.edgar.yodgorbekkomilo.newsapp.Adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.edgar.yodgorbekkomilo.newsapp.Pojo.Article;
+import com.edgar.yodgorbekkomilo.newsapp.R;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by yodgorbekkomilov on 2/18/18.
+ */
+
+public class ArticleAdapter extends ArrayList<Article> {
+
+    List<Article> articleList;
+    Context context;
+    private LayoutInflater mInflater;
+
+    // Constructors
+    public ArticleAdapter(Context context, List<Article> objects) {
+        super(context, 0, objects);
+        this.context = context;
+        this.mInflater = LayoutInflater.from(context);
+        articleList = objects;
+    }
+
+    @Override
+    public Article getItem(int position) {
+        return articleList.get(position);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        final ViewHolder vh;
+        if (convertView == null) {
+            View view = mInflater.inflate(R.layout.layout_row_view, parent, false);
+            vh = ViewHolder.create((RelativeLayout) view);
+            view.setTag(vh);
+        } else {
+            vh = (ViewHolder) convertView.getTag();
+        }
+
+        Article item = getItem(position);
+
+        vh.textViewSource.setText((CharSequence) item.getSource());
+        vh.textViewAuthor.setText(item.getAuthor());
+        Picasso.with(context).load(item.getUrlToImage()).placeholder(R.mipmap.ic_launcher).error(R.mipmap.ic_launcher).into(vh.imageView);
+
+        return vh.rootView;
+    }
+
+    private static class ViewHolder {
+        public final RelativeLayout rootView;
+        public final ImageView imageView;
+        public final TextView textViewSource;
+        public final TextView textViewAuthor;
+
+        private ViewHolder(RelativeLayout rootView, ImageView imageView, TextView textViewSource, TextView textViewAuthor) {
+            this.rootView = rootView;
+            this.imageView = imageView;
+            this.textViewSource = textViewSource;
+            this.textViewAuthor = textViewAuthor;
+        }
+
+        public static ViewHolder create(RelativeLayout rootView) {
+            ImageView imageView = (ImageView) rootView.findViewById(R.id.imageView);
+            TextView textViewSource = (TextView) rootView.findViewById(R.id.textViewSource);
+            TextView textViewAuthor = (TextView) rootView.findViewById(R.id.textViewAuthor);
+            return new ViewHolder(rootView, imageView, textViewSource, textViewAuthor);
+        }
+    }
+}
+
