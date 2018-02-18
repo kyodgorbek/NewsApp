b27c6c003edb1319b1ca2d3b45d62ab8056b9ad4
kyodgorbek <kyodgorbek@gmail.com>
2018-02-18 12:23:05 +0100

    Initial commit

---
+++ app/src/main/java/com/edgar/yodgorbekkomilo/newsapp/Pojo/News.java
@@ -0,0 +1,47 @@
+package com.edgar.yodgorbekkomilo.newsapp.Pojo;
+
+import java.util.List;
+
+/**
+ * Created by yodgorbekkomilov on 2/18/18.
+ */
+
+public class News {
+
+    @SerializedName("status")
+    @Expose
+    private String status;
+    @SerializedName("totalResults")
+    @Expose
+    private Integer totalResults;
+    @SerializedName("articles")
+    @Expose
+    private List<Article> articles = null;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Integer getTotalResults() {
+        return totalResults;
+    }
+
+    public void setTotalResults(Integer totalResults) {
+        this.totalResults = totalResults;
+    }
+
+    public List<Article> getArticles() {
+        return articles;
+    }
+
+    public void setArticles(List<Article> articles) {
+        this.articles = articles;
+    }
+
+}
+
+
---
+++ app/src/main/java/com/edgar/yodgorbekkomilo/newsapp/Pojo/Source.java
@@ -0,0 +1,35 @@
+package com.edgar.yodgorbekkomilo.newsapp.Pojo;
+
+/**
+ * Created by yodgorbekkomilov on 2/18/18.
+ */
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Source {
+
+    @SerializedName("id")
+    @Expose
+    private Object id;
+    @SerializedName("name")
+    @Expose
+    private String name;
+
+    public Object getId() {
+        return id;
+    }
+
+    public void setId(Object id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
