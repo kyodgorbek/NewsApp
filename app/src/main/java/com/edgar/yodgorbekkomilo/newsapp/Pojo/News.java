572f60211044c0a3e49178a7207c97100f4440c6
kyodgorbek <kyodgorbek@gmail.com>
2018-02-20 16:56:30 +0100

    I have added permission to android manifestzaxaxax

--- app/src/main/java/com/edgar/yodgorbekkomilo/newsapp/Pojo/News.java
+++ app/src/main/java/com/edgar/yodgorbekkomilo/newsapp/Pojo/News.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 public class News {
 
-    @SerializedName("status")
+    @SerializedName("status") 
     @Expose
     private String status;
     @SerializedName("totalResults")
