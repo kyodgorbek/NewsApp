4d8e08448f6e1679d908055fa53f60e181400c40
kyodgorbek <kyodgorbek@gmail.com>
2018-02-21 08:03:55 +0100

    I have added permission to android manifestzaxaxax

---
+++ app/src/main/java/com/edgar/yodgorbekkomilo/newsapp/AllNewsFragmentTab.java
@@ -0,0 +1,19 @@
+package com.edgar.yodgorbekkomilo.newsapp;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by yodgorbekkomilov on 2/21/18.
+ */
+
+public class AllNewsFragmentTab extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.all_news_fragment_tab, container, false);
+    }
+}
---
+++ app/src/main/java/com/edgar/yodgorbekkomilo/newsapp/PageAdapter.java
@@ -0,0 +1,44 @@
+package com.edgar.yodgorbekkomilo.newsapp;
+
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+
+/**
+ * Created by yodgorbekkomilov on 2/21/18.
+ */
+
+public class PageAdapter extends FragmentStatePagerAdapter {
+    int mNumOfTabs;
+
+    public PageAdapter(FragmentManager fm, int NumOfTabs) {
+        super(fm);
+        this.mNumOfTabs = NumOfTabs;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+
+        switch (position) {
+            case 0:
+                AllNewsFragmentTab allNewsFragmentTab = new AllNewsFragmentTab();
+                return allNewsFragmentTab;
+            case 1:
+                TechNewsFragmentTab techNewsFragmentTab = new TechNewsFragmentTab();
+                return techNewsFragmentTab;
+            case 2:
+                SportNewsFragmentTab sportNewsFragmentTab = new SportNewsFragmentTab();
+                return sportNewsFragmentTab;
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return mNumOfTabs;
+    }
+}
+
