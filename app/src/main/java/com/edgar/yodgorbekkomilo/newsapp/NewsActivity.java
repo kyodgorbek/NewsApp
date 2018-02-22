359cb7bb1d93605fd86856a665bf6dcea6852779
kyodgorbek <kyodgorbek@gmail.com>
2018-02-22 02:29:21 +0100

    I have added permission to android manifestzaxaxax

--- app/src/main/java/com/edgar/yodgorbekkomilo/newsapp/NewsActivity.java
+++ app/src/main/java/com/edgar/yodgorbekkomilo/newsapp/NewsActivity.java
@@ -1,7 +1,9 @@
 package com.edgar.yodgorbekkomilo.newsapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.TextView;
 
 /**
@@ -17,5 +19,12 @@ public class NewsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.news);
         TextView textView = (TextView)findViewById(R.id.News);
+
+        textView.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent intent = new Intent(NewsActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
 }
     }
