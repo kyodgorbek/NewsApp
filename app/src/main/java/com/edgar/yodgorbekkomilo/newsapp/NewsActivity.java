25ee806c5438a7c23259d6295a25134d4aacafa2
kyodgorbek <kyodgorbek@gmail.com>
2018-02-22 02:04:18 +0100

    I have added permission to android manifestzaxaxax

--- app/src/main/java/com/edgar/yodgorbekkomilo/newsapp/NewsActivity.java
+++ app/src/main/java/com/edgar/yodgorbekkomilo/newsapp/NewsActivity.java
@@ -1,8 +1,21 @@
 package com.edgar.yodgorbekkomilo.newsapp;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
 /**
  * Created by yodgorbekkomilov on 2/22/18.
  */
 
-public class NewsActivity {
+public class NewsActivity extends AppCompatActivity {
+
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.news);
+        TextView textView = (TextView)findViewById(R.id.News);
 }
+    }
