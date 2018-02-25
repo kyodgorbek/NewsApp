e9dec4b801051aac832232cf68a7a8a3a2819ec8
kyodgorbek <kyodgorbek@gmail.com>
2018-02-25 16:36:21 +0100

    I have added permission to android manifestzaxaxax

---
+++ app/src/main/java/com/edgar/yodgorbekkomilo/newsapp/NewsDetailActivity.java
@@ -0,0 +1,17 @@
+package com.edgar.yodgorbekkomilo.newsapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by yodgorbekkomilov on 2/25/18.
+ */
+
+public class NewsDetailActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.news_details);
+    }
+}
