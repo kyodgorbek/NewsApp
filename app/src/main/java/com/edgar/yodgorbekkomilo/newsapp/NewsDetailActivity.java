08c61cab8616fb262206e0b9f6ba7d647d145d3a
kyodgorbek <kyodgorbek@gmail.com>
2018-02-26 17:26:36 +0100

    I have added permission to android manifestzaxaxax

--- app/src/main/java/com/edgar/yodgorbekkomilo/newsapp/NewsDetailActivity.java
+++ app/src/main/java/com/edgar/yodgorbekkomilo/newsapp/NewsDetailActivity.java
@@ -3,6 +3,8 @@ package com.edgar.yodgorbekkomilo.newsapp;
 import android.app.Activity;
 import android.os.Bundle;
 
+import com.edgar.yodgorbekkomilo.newsapp.Pojo.Article;
+
 /**
  * Created by yodgorbekkomilov on 2/25/18.
  */
@@ -13,5 +15,7 @@ public class NewsDetailActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.news_details);
+
+        Article article = (Article) getIntent().getParcelableExtra("myDataKey");
     }
 }
