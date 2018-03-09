754833d776b6af3456a6f8e7305cc3354417bc2c
kyodgorbek <kyodgorbek@gmail.com>
2018-03-09 20:40:37 +0100

    I have  implemented content provider.query method

--- app/src/main/java/com/edgar/yodgorbekkomilo/newsapp/MainActivity.java
+++ app/src/main/java/com/edgar/yodgorbekkomilo/newsapp/MainActivity.java
@@ -33,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
                 tabLayout.addTab(tabLayout.newTab().setText("AllNews"));
                 tabLayout.addTab(tabLayout.newTab().setText(" TechNews"));
                 tabLayout.addTab(tabLayout.newTab().setText("SportNews"));
+                tabLayout.addTab(tabLayout.newTab().setText("Favorite"));
                 tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
 
                 final ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
@@ -71,26 +72,7 @@ public class MainActivity extends AppCompatActivity {
                         return true;
                 }
 
-
-
-
                 return false;
         }
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
