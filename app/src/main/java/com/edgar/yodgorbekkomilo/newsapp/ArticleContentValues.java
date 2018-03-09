1222aa6e87e932096c250ec2a648102a1b30a7ea
kyodgorbek <kyodgorbek@gmail.com>
2018-03-09 20:30:15 +0100

    I have  implemented content provider.query method

--- app/src/main/java/com/edgar/yodgorbekkomilo/newsapp/ArticleContentValues.java
+++ app/src/main/java/com/edgar/yodgorbekkomilo/newsapp/ArticleContentValues.java
@@ -14,59 +14,63 @@ import com.edgar.yodgorbekkomilo.newsapp.Pojo.Article;
 
 public class ArticleContentValues extends AbstractContentValues {
 
-        @Override
-        public Uri uri() {
-            return ArticleColumns.CONTENT_URI;
-        }
-
-        /**
-         * Update row(s) using the values stored by this object and the given selection.
-         *
-         * @param contentResolver The content resolver to use.
-         * @param where           The selection to use (can be {@code null}).
-         */
-        public int update(ContentResolver contentResolver, @Nullable ArticleSelection where) {
-            return contentResolver.update(uri(), values(), where == null ? null : where.sel(), where == null ? null : where.args());
-        }
-
-        /**
-         * Update row(s) using the values stored by this object and the given selection.
-         *
-         * @param context The context to use.
-         * @param where   The selection to use (can be {@code null}).
-         */
-        public int update(Context context, @Nullable ArticleSelection where) {
-            return context.getContentResolver().update(uri(), values(), where == null ? null : where.sel(), where == null ? null : where.args());
-        }
-
-        public ArticleContentValues putID(@NonNull long value) {
-//        if (value == null) throw new IllegalArgumentException("title must not be null");
-            mContentValues.put(ArticleColumns._ID, value);
-            return this;
-        }
-
-        public ArticleContentValues putTitle(@NonNull String value) {
-            if (value == null) throw new IllegalArgumentException("title must not be null");
-            mContentValues.put(ArticleColumns.TITLE, value);
-            return this;
-        }
-
-
-        public ArticleContentValues putTitleDescripton(@Nullable String value) {
-            mContentValues.put(ArticleColumns.TITLE_DESCRIPTION, value);
-            return this;
-        }
-
-        public ArticleContentValues putAuthor() {
-            mContentValues.putNull(ArticleColumns.AUTHOR);
-            return this;
-        }
-
-        public ArticleContentValues putISFavorite(@Nullable String value) {
-            mContentValues.put(ArticleColumns.IS_FAVOURITE, value);
-            return this;
-        }
-
-
+    @Override
+    public Uri uri() {
+        return ArticleColumns.CONTENT_URI;
     }
+
+    /**
+     * Update row(s) using the values stored by this object and the given selection.
+     *
+     * @param contentResolver The content resolver to use.
+     * @param where           The selection to use (can be {@code null}).
+     */
+    public int update(ContentResolver contentResolver, @Nullable ArticleSelection where) {
+        return contentResolver.update(uri(), values(), where == null ? null : where.sel(), where == null ? null : where.args());
+    }
+
+    /**
+     * Update row(s) using the values stored by this object and the given selection.
+     *
+     * @param context The context to use.
+     * @param where   The selection to use (can be {@code null}).
+     */
+    public int update(Context context, @Nullable ArticleSelection where) {
+        return context.getContentResolver().update(uri(), values(), where == null ? null : where.sel(), where == null ? null : where.args());
+    }
+
+    public ArticleContentValues putID(@NonNull long value) {
+//        if (value == null) throw new IllegalArgumentException("title must not be null");
+        mContentValues.put(ArticleColumns._ID, value);
+        return this;
+    }
+
+    public ArticleContentValues putTitle(@NonNull String value) {
+        if (value == null) throw new IllegalArgumentException("title must not be null");
+        mContentValues.put(ArticleColumns.TITLE, value);
+        return this;
+    }
+
+    public ArticleContentValues putImage(@Nullable String value) {
+        mContentValues.put(ArticleColumns.IMAGE, value);
+        return this;
+    }
+
+    public ArticleContentValues putTitleDescripton(@Nullable String value) {
+        mContentValues.put(ArticleColumns.TITLE_DESCRIPTION, value);
+        return this;
+    }
+
+    public ArticleContentValues putAuthor() {
+        mContentValues.putNull(ArticleColumns.AUTHOR);
+        return this;
+    }
+
+    public ArticleContentValues putISFavorite(@Nullable String value) {
+        mContentValues.put(ArticleColumns.IS_FAVOURITE, value);
+        return this;
+    }
+
+
+}
 
