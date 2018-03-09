efbf5734e6a61993034437e708e6b952da8449b2
kyodgorbek <kyodgorbek@gmail.com>
2018-03-09 13:35:36 +0100

    I have  implemented content provider.query method

--- app/src/main/java/com/edgar/yodgorbekkomilo/newsapp/FavoriteArticlesFragment.java
+++ app/src/main/java/com/edgar/yodgorbekkomilo/newsapp/FavoriteArticlesFragment.java
@@ -1,8 +1,11 @@
 package com.edgar.yodgorbekkomilo.newsapp;
 
 import android.app.Fragment;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,6 +19,17 @@ public class FavoriteArticlesFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_articles_favorite, container, false);
+
+        String queryUri = ArticleColumns.CONTENT_URI.toString();
+        Cursor cursor =
+                getActivity().getApplicationContext().getContentResolver().query(Uri.parse(queryUri), null, null,
+                        null, null);
+
+        if (cursor != null) {
+            if (cursor.getCount() > 0) {
+                Log.i("favorites", cursor.toString);
+            }
+        }
 
         return view;
     }
