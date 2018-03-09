e93629089704dfdc5027c74614e0dd94a5933191
kyodgorbek <kyodgorbek@gmail.com>
2018-03-09 11:25:21 +0100

    I have started to implement content provider

---
+++ app/src/main/java/com/edgar/yodgorbekkomilo/newsapp/FavoriteArticlesFragment.java
@@ -0,0 +1,14 @@
+package com.edgar.yodgorbekkomilo.newsapp;
+
+import android.app.Fragment;
+
+/**
+ * Created by yodgorbekkomilov on 3/9/18.
+ */
+
+public class FavoriteArticlesFragment extends Fragment {
+
+
+
+
+}
