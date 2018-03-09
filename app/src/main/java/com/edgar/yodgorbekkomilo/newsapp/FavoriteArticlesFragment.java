76055ac8057224987c36f378b66e706bb652b7ad
kyodgorbek <kyodgorbek@gmail.com>
2018-03-09 13:20:13 +0100

    I have started to implement content provider

--- app/src/main/java/com/edgar/yodgorbekkomilo/newsapp/FavoriteArticlesFragment.java
+++ app/src/main/java/com/edgar/yodgorbekkomilo/newsapp/FavoriteArticlesFragment.java
@@ -1,6 +1,11 @@
 package com.edgar.yodgorbekkomilo.newsapp;
 
 import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by yodgorbekkomilov on 3/9/18.
@@ -8,7 +13,10 @@ import android.app.Fragment;
 
 public class FavoriteArticlesFragment extends Fragment {
 
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_articles_favorite, container, false);
 
-
-
+        return view;
+    }
 }
