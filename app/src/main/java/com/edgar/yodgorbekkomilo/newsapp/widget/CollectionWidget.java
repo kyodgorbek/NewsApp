6c056f0ea91383d04ab03c20b8eba5922b57346c
kyodgorbek <kyodgorbek@gmail.com>
2018-03-13 14:18:16 +0100

    I have  implemented widget provier

--- app/src/main/java/com/edgar/yodgorbekkomilo/newsapp/widget/CollectionWidget.java
+++ app/src/main/java/com/edgar/yodgorbekkomilo/newsapp/widget/CollectionWidget.java
@@ -34,7 +34,7 @@ public class CollectionWidget extends AppWidgetProvider {
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        // There may be multiple widgets active, so update all of them
+        // There may be multiple widgetas active, so update all of them
         for (int appWidgetId : appWidgetIds) {
             updateAppWidget(context, appWidgetManager, appWidgetId);
         }
--- app/src/main/java/com/edgar/yodgorbekkomilo/newsapp/widget/WidgetDataProvider.java
+++ app/src/main/java/com/edgar/yodgorbekkomilo/newsapp/widget/WidgetDataProvider.java
@@ -1,9 +1,17 @@
 package com.edgar.yodgorbekkomilo.newsapp.widget;
 
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.os.Bundle;
 import android.widget.RemoteViews;
 import android.widget.RemoteViewsService;
+
+import com.edgar.yodgorbekkomilo.newsapp.R;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,70 +20,89 @@ import java.util.List;
  * Created by yodgorbekkomilov on 3/11/18.
  */
 
-public class WidgetDataProvider implements RemoteViewsService.RemoteViewsFactory {
+public class WidgetDataProvider extends AppWidgetProvider{
 
-    private static final String TAG = "WidgetDataProvider";
 
-    List<String> mCollection = new ArrayList<>();
-    Context mContext = null;
 
-    public WidgetDataProvider(Context context, Intent intent) {
-        mContext = context;
-    }
 
+    public static String MY_WIDGET_UPDATE = "android.appwidget.action.MY_OWN_WIDGET_UPDATE";
     @Override
-    public void onCreate() {
-        initData();
-    }
+    public void onReceive(Context context, Intent intent) {
+        // TODO Auto-generated method stub
+        super.onReceive(context, intent);
 
-    @Override
-    public void onDataSetChanged() {
-        initData();
-    }
+        if(MY_WIDGET_UPDATE.equals(intent.getAction())){
 
-    @Override
-    public void onDestroy() {
+            Bundle extras = intent.getExtras();
+            if(extras!=null) {
+                AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+                ComponentName thisAppWidget = new ComponentName(context.getPackageName(), WidgetDataProvider.class.getName());
+                int[] appWidgetIds = appWidgetManager.getAppWidgetIds(thisAppWidget);
 
-    }
+                onUpdate(context, appWidgetManager, appWidgetIds);
+            }
 
-    @Override
-    public int getCount() {
-        return mCollection.size();
-    }
-
-    @Override
-    public RemoteViews getViewAt(int position) {
-        RemoteViews view = new RemoteViews(mContext.getPackageName(),
-                android.R.layout.simple_list_item_1);
-        view.setTextViewText(android.R.id.text1, mCollection.get(position));
-        return view;
-    }
-
-    @Override
-    public RemoteViews getLoadingView() {
-        return null;
-    }
-
-    @Override
-    public int getViewTypeCount() {
-        return 1;
-    }
-
-    @Override
-    public long getItemId(int position) {
-        return position;
-    }
-
-    @Override
-    public boolean hasStableIds() {
-        return true;
-    }
-
-    private void initData() {
-        mCollection.clear();
-        for (int i = 1; i <= 10; i++) {
-            mCollection.add("ListView item " + i);
+            //Toast.makeText(context, "onReceiver()", Toast.LENGTH_LONG).show();
         }
     }
 
+
+    public void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+                                int[] appWidgetId) {
+
+
+        final int N = appWidgetId.length;
+        for (int i = 0; i < N; i++) {
+            RemoteViews remoteViews = updateAppWidgetListView(context, appWidgetId[i]);
+            appWidgetManager.updateAppWidget(appWidgetId[i], remoteViews);
+        }
+        super.onUpdate(context, appWidgetManager, appWidgetId);
+    }
+
+        RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.collection_widget);
+        Intent svcIntent = new Intent(context, WidgetService.class);
+
+        svcIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
+        svcIntent.setData(Uri.parse(svcIntent.toUri(Intent.URI_INTENT_SCHEME)));
+        remoteViews.setRemoteAdapter(appWidgetId, R.id., svcIntent);
+        remoteViews.setEmptyView(com.edgar.yodgorbekkomilov.yodgorbekkomilov.R.id.listViewWidget, com.edgar.yodgorbekkomilov.yodgorbekkomilov.R.id.empty_view);
+
+
+
+
+  return remoteViews;
+ 
+
 }
+
+
+
+
+    // Instruct the widget manager to update the widget
+    //  appWidgetManager.updateAppWidget(appWidgetId, views);
+
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetId) {
+        SharedPreferences sharedPreferences = context.getSharedPreferences("MyIngredients", Context.MODE_PRIVATE);
+
+        // RemoteViewsService.RemoteViewsFactory remoteViewsFactory
+
+
+        // There may be multiple widgets active, so update all of them
+        for (int appWidgetIds : appWidgetId) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
+        }
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+}
+
