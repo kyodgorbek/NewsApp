f36df2691ad0b245836a7126a0de9ed61de4c0ce
kyodgorbek <kyodgorbek@gmail.com>
2018-03-15 21:42:29 +0100

    I have started to implement SyncAdapter

---
+++ app/src/main/java/com/edgar/yodgorbekkomilo/newsapp/syncAdapter/AuthenticatorService.java
@@ -0,0 +1,28 @@
+package com.edgar.yodgorbekkomilo.newsapp.syncAdapter;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by yodgorbekkomilov on 3/15/18.
+ */
+
+public class AuthenticatorService extends Service {
+    private AccountAuthenticator authenticator;
+
+
+    @Override
+    public void onCreate() {
+        // Instantiate our authenticator when the service is created
+        this.authenticator = new AccountAuthenticator(this);
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        // Return the authenticator's IBinder
+        return authenticator.getIBinder();
+    }
+}
