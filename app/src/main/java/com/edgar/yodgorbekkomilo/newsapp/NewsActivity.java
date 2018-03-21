1386d09664beba536057a3544b982a62bd2557fd
kyodgorbek <kyodgorbek@gmail.com>
2018-03-21 10:29:18 +0100

    I started to implement CursorLoader in android

--- app/src/main/java/com/edgar/yodgorbekkomilo/newsapp/NewsActivity.java
+++ app/src/main/java/com/edgar/yodgorbekkomilo/newsapp/NewsActivity.java
@@ -35,10 +35,10 @@ public class NewsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.news);
- 
+        new MyAsyncTask().execute();
         ImageView imageView = (ImageView) findViewById(R.id.imageViewBackground);
         TextView textView = (TextView)findViewById(R.id.News) ;
-        new MyAsyncTask().execute();
+
 
         textView.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
